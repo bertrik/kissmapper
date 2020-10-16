374254a04517968768391107f1d1ae4335334a92
Bertrik Sikken <bertrik@sikken.nl>
2020-10-16 19:22:44 +0200

    Run autopep8 on python script.

--- responder.py
+++ responder.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import base64
 
+
 class Responder():
     def __init__(self, server, username, password):
         self.server = server
@@ -14,7 +15,7 @@ class Responder():
         self.client.username_pw_set(username, password)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        
+
     def rssi2color(self, rssi):
         if (rssi > -100):
             return bytes([0xFF, 0x00, 0x00])
@@ -31,18 +32,18 @@ class Responder():
     def on_message(self, client, userdata, msg):
         try:
             print(f"Receive on topic {msg.topic}:")
-            
+
             # decode
             up = json.loads(msg.payload)
             print(f"{up}")
-            
+
             # calculate max rssi
-            maxrssi = -1000;
+            maxrssi = -1000
             for g in up['metadata']['gateways']:
                 rssi = g["rssi"]
                 if rssi > maxrssi:
                     maxrssi = rssi
-            
+
             # check button press
             payload_raw = base64.b64decode(up["payload_raw"])
             if ((payload_raw[0] & 128) != 0):
@@ -57,10 +58,10 @@ class Responder():
                 client.publish(topic, json.dumps(down))
         except Exception as e:
             print("Caught exception:", e)
-        
+
     def on_connect(self, client, userdata, flags, rc):
         try:
-            topic = "+/devices/+/up";
+            topic = "+/devices/+/up"
             print(f"Subscribing to uplink topic {topic}...")
             client.subscribe(topic)
         except Exception as e:
@@ -72,18 +73,21 @@ class Responder():
         while True:
             self.client.loop()
 
+
 def main():
     """ The main entry point """
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--server", help="The MQTT server, e.g. eu.thethings.network",
                         default="eu.thethings.network")
-    parser.add_argument("-u", "--username", help = "The MQTT user name (TTN application name)", default = "lorakiss-app", required = True)
-    parser.add_argument("-p", "--password", help = "The MQTT password (TTN access key)", default="ttn-account-v2.wsHo81XnbhmQ5NJYm6UCHhfupunktulDH-1AXVE3YI4")
+    parser.add_argument("-u", "--username", help="The MQTT user name (TTN application name)",
+                        default="lorakiss-app", required=True)
+    parser.add_argument("-p", "--password", help="The MQTT password (TTN access key)",
+                        default="ttn-account-v2.wsHo81XnbhmQ5NJYm6UCHhfupunktulDH-1AXVE3YI4")
     args = parser.parse_args()
 
     responder = Responder(args.server, args.username, args.password)
     responder.run()
 
+
 if __name__ == "__main__":
     main()
-    
